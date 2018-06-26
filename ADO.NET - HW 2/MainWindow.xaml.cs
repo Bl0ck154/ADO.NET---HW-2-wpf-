059adee2f95b32b4698a9d7f4256e11ece5d172e
Bl0ck <zail@dublin.com>
2018-06-26 15:13:47 +0300

    Add project files.

---
+++ ADO.NET - HW 2/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO.NET___HW_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		SqlConnection sqlConnection;
+		SqlDataAdapter sqlDataAdapter;
+		DataSet dataSet;
+
+		public MainWindow()
+        {
+            InitializeComponent();
+			Loaded += MainWindow_Loaded;
+			sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["mainConnect"].ConnectionString);
+			
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+		}
+
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				sqlConnection.Open();
+
+				try // first use create table Users
+				{
+					new SqlCommand(@"create table Users ( 
+id INT not null primary key identity,
+login varchar(32) not null,
+password varchar(32) not null, 
+address varchar(100), 
+phone varchar(16),
+admin BIT default 0)", sqlConnection).ExecuteNonQuery();
+				}
+				catch (Exception)
+				{
+				}
+
+				 sqlDataAdapter = new SqlDataAdapter("select * from Users", sqlConnection);
+				dataSet = new DataSet();
+				sqlDataAdapter.Fill(dataSet, "Users");
+				foreach (DataRow row in dataSet.Tables["Users"].Rows)
+				{
+					ListBoxItem lbi = new ListBoxItem();
+					lbi.MouseDoubleClick += UsersListbox_MouseDoubleClick;
+
+					string tmp = "";
+					foreach (var item in row.ItemArray)
+					{
+						tmp  += item + " ";
+
+					}
+					lbi.Content = tmp;
+
+					UsersListbox.Items.Add(lbi);
+				
+				}
+				
+				//UsersListbox.ItemsSource = dataSet.Tables["Users"].DefaultView;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				if(sqlConnection.State == ConnectionState.Open)
+					sqlConnection.Close();
+			}
+
+		}
+
+		private void UsersListbox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			if(UsersListbox.SelectedItems.Count > 0)
+				MessageBox.Show(UsersListbox.SelectedItem.ToString());
+		}
+
+		private void CreateButton_Click(object sender, RoutedEventArgs e)
+		{
+			if(new UserEdit().ShowDialog() == true)
+			{
+				sqlDataAdapter.Update(dataSet);
+			}
+		}
+
+		private void RemoveButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void UsersListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			RemoveButton.IsEnabled = UsersListbox.SelectedItems.Count == 0 ? false : true;
+		}
+	}
+}
---
+++ ADO.NET - HW 2/UserEdit.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ADO.NET___HW_2
+{
+	/// <summary>
+	/// Interaction logic for UserEdit.xaml
+	/// </summary>
+	public partial class UserEdit : Window
+	{
+		public UserEdit()
+		{
+			InitializeComponent();
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+		}
+		//public UserEdit(/*params*/) 
+		//{
+
+		//}
+
+		private void CreateButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				using (SqlConnection sqlConnection =
+					new SqlConnection(ConfigurationManager.ConnectionStrings["mainConnect"].ConnectionString))
+				{
+					sqlConnection.Open();
+					SqlCommand command = new SqlCommand(@"insert into Users VALUES (@login, @password, @address, @phone, " +
+						(AdminFlag.IsChecked == true ? "1)" : "0)"), sqlConnection);
+					command.Parameters.AddWithValue("@login", LoginTextbox.Text);
+					command.Parameters.AddWithValue("@password", PasswordTextbox.Password.GetHashCode());
+					command.Parameters.AddWithValue("@address", AddressTextbox.Text);
+					command.Parameters.AddWithValue("@phone", PhoneTextbox.Text);
+					command.ExecuteNonQuery();
+					DialogResult = true;
+					Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void LoginTextbox_TextChanged(object sender, RoutedEventArgs e)
+		{
+			CreateButton.IsEnabled = LoginTextbox.Text.Length > 0 && PasswordTextbox.Password.Length > 0 ? true : false;
+		}
+	}
+}
